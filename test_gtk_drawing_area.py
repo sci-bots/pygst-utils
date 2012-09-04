8557d35441924a857f4964c2dda02f41722b3662
Christian Fobel <christian@fobel.net>
2012-09-03 23:42:28 -0400

    Modify test_gtk_drawing_area to use GStreamerVi...
    Modify test_gtk_drawing_area to use GStreamerVideoView

--- test_gtk_drawing_area.py
+++ test_gtk_drawing_area.py
@@ -46,34 +46,8 @@ class GTK_Main:
         self.aframe = gtk.AspectFrame(xalign=0.5, yalign=1.0, ratio=4.0 / 3.0,
                 obey_child=False)
         
-        self.pipeline = gst.Pipeline('pipeline')
-
-        camera_bin = gst.element_factory_make('camerabin', 'camera_bin')
-        warp_bin = WarpBin('warp_bin')
-
-        camera_bin.set_property('viewfinder-filter', warp_bin)
-
-        # Disable audio
-        flags = camera_bin.get_property('flags')
-        camera_bin.set_property('flags', flags | 0x020)
-
-        # Set mode to video (rather than image)
-        camera_bin.set_property('mode', 1)
-
-        # Set video source to test source
-        camera_bin.set_property('video-source', self.get_test_src())
-        self.record_enabled = False
-
-        # Set recording format to mpeg4 avi
-        avi_mux = gst.element_factory_make('avimux', 'avi_mux')
-        #ffenc_mpeg4 = gst.element_factory_make('ffenc_mpeg4', 'ffenc_mpeg40') 
-        ffenc_mpeg4 = gst.element_factory_make('xvidenc', 'ffenc_mpeg40') 
-        #ffenc_mpeg4.set_property('bitrate', 1200000)
-
-        camera_bin.set_property('video-muxer', avi_mux)
-        camera_bin.set_property('video-encoder', ffenc_mpeg4)
-
-        self.pipeline.add(camera_bin)
+        from test_video import get_pipeline, get_auto_src
+        self.pipeline = get_pipeline()
 
         self.movie_view = GStreamerVideoView(self.pipeline)
         self.movie_window = self.movie_view.widget
@@ -82,52 +56,14 @@ class GTK_Main:
         vbox.pack_start(self.aframe, False)
         window.show_all()
         self.window = window
-        self.pipeline.set_state(gst.STATE_PLAYING)
-
-    def get_auto_src(self):
-        #return RatedBin('video_src')
-        video_source = GstVideoSourceManager.get_video_source()
-        selected_mode = video_modes[0]
-        video_source.set_property(device_key, selected_mode['device'])
-        caps_str = GstVideoSourceManager.get_caps_string(selected_mode)
-        filtered_input = FilteredInput('video_src', caps_str, video_source)
-        return filtered_input
-
-    def get_test_src(self):
-        video_src = gst.element_factory_make('videotestsrc', 'video_src')
-        video_src.set_property('pattern', 2)
-        return RatedBin('video_src', video_src=video_src)
 
     def start_stop(self, w):
         if self.button.get_label() == "Start":
-            camera_bin = self.pipeline.get_by_name('camera_bin')
-            if not self.record_enabled:
-                self.pipeline.set_state(gst.STATE_NULL)
-                camera_bin.set_property('video-source', self.get_auto_src())
-                self.pipeline.set_state(gst.STATE_PLAYING)
-                self.record_enabled = True
-            transform_str = self.entry.get_text()
-            if transform_str:
-                warp_bin = camera_bin.get_by_name('warp_bin')
-                warp_bin.warper.set_property('transform_matrix', transform_str)
-            ready = False
-            for i in range(5):
-                if camera_bin.get_property('ready-for-capture'):
-                    ready = True
-                    break
-                time.sleep(0.1)
-            if not ready:
-                raise RuntimeError, 'camerabin is not ready for capture'
-            camera_bin.set_property('filename', 'test.avi')
-            camera_bin.emit('capture-start')
+            self.pipeline.set_state(gst.STATE_PLAYING)
 
             self.button.set_label("Stop")
         else:
-            camera_bin = self.pipeline.get_by_name('camera_bin')
-            camera_bin.emit('capture-stop')
             self.pipeline.set_state(gst.STATE_NULL)
-            camera_bin.set_property('video-source', self.get_test_src())
-            self.pipeline.set_state(gst.STATE_PLAYING)
             self.button.set_label("Start")
 
     def on_destroy(self, *args):
