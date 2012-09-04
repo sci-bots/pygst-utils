ac2bf81e4ad52822de56f9d2bd9b7d3eed24f5a8
Christian Fobel <christian@fobel.net>
2012-09-04 16:32:30 -0400

    Specify bitrate in KB/s instead of Kbit/s

--- test_gtk_drawing_area.py
+++ test_gtk_drawing_area.py
@@ -61,9 +61,9 @@ class GTK_Main:
         form = Form.of(get_video_mode_enum().using(
                 default=self.video_mode_keys[0]), Filepath.named('output_path')\
                         .using(default=''), Integer.named('bitrate').using(
-                                default=1200, validators=[ValueAtLeast(
-                                        minimum=25)], properties={'step': 100, 
-                                                'label': 'bitrate (Kbps)', }))
+                                default=150, validators=[ValueAtLeast(
+                                        minimum=25)], properties={'step': 25, 
+                                                'label': 'Bitrate (KB/s)', }))
         self.video_mode_form_view = create_form_view(form)
         for field in ['video_mode', 'output_path', 'bitrate']:
             setattr(self, '%s_field' % field, self.video_mode_form_view.form\
@@ -86,7 +86,7 @@ class GTK_Main:
 
     @property
     def bitrate(self):
-        return (self.video_mode_form_view.form.fields['bitrate'].element.value << 10)
+        return (self.video_mode_form_view.form.fields['bitrate'].element.value << 13)
 
     @property
     def output_path(self):
