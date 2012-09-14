7870bd72f251de12ec39e6c54cc360ecca1fb29b
Christian Fobel <christian@fobel.net>
2012-09-14 02:42:55 -0400

    Add server_popen() function to server.py
    Add examples directory.  The gtk_gstreamer_window.py example
    demonstrates running a WindowService server in a Popen subprocess.  The
    created GStreamer video sink draws to a gtk.DrawingArea widget.
    
    Note that through the use of a subprocess to run the GStreamer code, all
    GStreamer processing is completely decoupled from the GTK view.

---
+++ examples/gtk_gstreamer_window/gtk_gstreamer_window.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+
+import cgi
+import logging
+import sys, os
+import pygtk, gtk
+import time
+if os.name == 'nt':
+    import win32com
+    import pythoncom
+
+try:
+    import pygst
+    pygst.require("0.10")
+except ImportError:
+    pass
+finally:
+    import gst
+from jsonrpclib import Server
+import blinker
+import decimal
+from path import path
+
+import pygst_utils
+from pygst_utils.video_view.gtk_view import GtkVideoView
+from pygst_utils.bin.server import server_popen
+from gst_video_source_caps_query.gst_video_source_caps_query import\
+        DeviceNotFound, GstVideoSourceManager, FilteredInput
+from gst_video_source_caps_query.video_mode_dialog import create_form_view,\
+        get_video_mode_map, get_video_mode_enum, get_available_video_modes,\
+                get_video_source_configs
+from pygtkhelpers.ui.extra_widgets import Filepath
+from pygtkhelpers.ui.dialogs import error
+from flatland import Form, Integer
+from flatland.validation import ValueAtLeast, ValueAtMost
+
+
+class GTKGStreamerWindow(object):
+    try:
+        video_modes = get_available_video_modes(
+                format_='YUY2')
+        video_mode_map = get_video_mode_map(video_modes)
+        video_mode_keys = sorted(video_mode_map.keys())
+        device_key, devices = get_video_source_configs()
+        if video_mode_keys:
+            _video_available = True
+        else:
+            _video_available = False
+    except DeviceNotFound:
+        _video_available = False
+
+    def __init__(self):
+        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        window.set_title("Mpeg2-Player")
+        window.set_default_size(640, 500)
+        window.connect("destroy", self.on_destroy)
+        vbox = gtk.VBox()
+        window.add(vbox)
+        hbox = gtk.HBox()
+        vbox.pack_start(hbox, expand=False)
+
+        form = Form.of(get_video_mode_enum().using(
+                default=self.video_mode_keys[0]), Filepath.named('output_path')\
+                        .using(default=''), Integer.named('bitrate').using(
+                                default=150, validators=[ValueAtLeast(
+                                        minimum=25)], properties={'step': 25,
+                                                'label': 'Bitrate (KB/s)', }))
+        self.video_mode_form_view = create_form_view(form)
+        for field in ['video_mode', 'output_path', 'bitrate']:
+            setattr(self, '%s_field' % field, self.video_mode_form_view.form\
+                    .fields[field])
+        self.video_mode_field.proxy.connect('changed', self._on_mode_changed)
+        self.video_source = None
+        # Set default transform to identity
+        hbox.add(self.video_mode_form_view.widget)
+        self.button = gtk.Button("Start")
+        hbox.pack_start(self.button, False)
+        self.button.connect("clicked", self.start_stop)
+        self.aframe = gtk.AspectFrame(xalign=0.5, yalign=1.0, ratio=4.0 / 3.0,
+                obey_child=False)
+
+        self.pipeline = None
+        self._server = None
+
+        vbox.pack_start(self.aframe, expand=True)
+        self.movie_view = GtkVideoView()
+        self.movie_window = self.movie_view.widget
+        self.aframe.add(self.movie_window)
+        window.show_all()
+        self.window = window
+
+    @property
+    def bitrate(self):
+        return (self.video_mode_form_view.form.fields['bitrate'].element.value << 13)
+
+    @property
+    def output_path(self):
+        return self.video_mode_form_view.form.fields['output_path'].element.value
+
+    @property
+    def video_settings(self):
+        return self.video_mode_form_view.form.fields['video_mode'].element.value
+
+    def _on_mode_changed(self, *args):
+        self.video_settings = self.video_mode_form_view.form.fields[
+                'video_mode'].element.value
+
+    @video_settings.setter
+    def video_settings(self, value):
+        self._video_settings = value
+
+    def get_video_device_and_caps_str(self):
+        selected_mode = self.video_mode_map[self.video_settings]
+        caps_str = GstVideoSourceManager.get_caps_string(selected_mode)
+        return (str(selected_mode['device']), caps_str)
+
+    def start_stop(self, w):
+        if self.button.get_label() == "Start":
+            self.start()
+        else:
+            self.stop()
+
+    def start(self):
+        if not self.output_path:
+            error('Please select a valid output filepath.')
+            return
+        self.movie_window.set_size_request(640, 480)
+        self.aframe.show_all()
+
+        # Start JSON-RPC server to control GStreamer video pipeline.
+        # There are issues with the GTK gui freezing when the
+        # GStreamer pipeline is started here directly.
+        self._server = server_popen()
+        time.sleep(1)
+        # Connect to JSON-RPC server and request to run the pipeline
+        s = Server('http://localhost:8080')
+
+        s.create_process(self.movie_view.window_xid)
+        s.create_pipeline(self.movie_view.window_xid,
+                self.get_video_device_and_caps_str(), self.output_path,
+                        self.bitrate)
+        s.start_pipeline(self.movie_view.window_xid)
+
+        self.video_mode_field.proxy.widget.set_button_sensitivity(gtk.SENSITIVITY_OFF)
+        self.output_path_field.widget.set_sensitive(False)
+        self.bitrate_field.widget.set_sensitive(False)
+
+        self.button.set_label("Stop")
+
+    def stop(self):
+        s = Server('http://localhost:8080')
+        s.stop_pipeline(self.movie_view.window_xid)
+        s.terminate_process(self.movie_view.window_xid)
+        self._server.kill()
+        self._server = None
+        #self.aframe.remove(self.movie_window)
+        #del self.movie_view
+        #self.movie_view = None
+        self.button.set_label("Start")
+        self.video_mode_field.proxy.widget.set_button_sensitivity(gtk.SENSITIVITY_AUTO)
+        self.output_path_field.widget.set_sensitive(True)
+        self.bitrate_field.widget.set_sensitive(True)
+
+    def on_destroy(self, *args):
+        if self._server:
+            self.stop()
+        gtk.main_quit()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='[%(levelname)s] %(message)s', loglevel=logging.DEBUG)
+    GTKGStreamerWindow()
+    gtk.main()
--- pygst_utils/bin/server.py
+++ pygst_utils/bin/server.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 import logging
+import sys
+from subprocess import Popen, PIPE
 
+from path import path
+import pygst_utils
 from pygst_utils.video_pipeline.window_service import WindowService
+
+
+def base_path():
+    return path(pygst_utils.__file__).parent.joinpath('bin')
+
+
+def server_popen():
+    if hasattr(sys, 'frozen'):
+        server_process = Popen([base_path().joinpath('server.exe')], stdout=PIPE, stderr=PIPE)
+    else:
+        server_process = Popen([sys.executable, base_path().joinpath('server.py')], stdout=PIPE, stderr=PIPE)
+    return server_process
 
 
 if __name__ == '__main__':
