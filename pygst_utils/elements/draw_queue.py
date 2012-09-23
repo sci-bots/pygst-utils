dfccf39fe3091edfcd5f512c81fcb08bc6a7eab6
Christian Fobel <christian@fobel.net>
2012-09-22 21:43:26 -0400

    Add cairo draw methods to DrawQueue
    Rather than appending "closure" instances manually to the DrawQueue,
    instead, expose cairo draw functions as DrawQueue methods.  This
    provides a natural API for recording cairo drawing commands.

--- pygst_utils/elements/draw_queue.py
+++ pygst_utils/elements/draw_queue.py
@@ -6,7 +6,48 @@ from collections import deque
 import cairo
 
 
-class DrawQueue:
+#### drawing closures
+class Paint(object):
+    def __call__(self, ctx):
+        ctx.paint()
+
+
+class Fill(object):
+    def __call__(self, ctx):
+        ctx.fill()
+
+
+class SetSourceRgb(object):
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
+
+    def __call__(self, ctx):
+        ctx.set_source_rgb(self.r, self.g, self.b)
+
+
+class MoveTo(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __call__(self, ctx):
+        ctx.move_to(self.x, self.y)
+
+
+class Rectangle(object):
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    def __call__(self, ctx):
+        ctx.rectangle(self.x, self.y, self.w, self.h)
+
+
+class DrawQueue(object):
     '''
     A list of draw commands, stored as callables that, are
     passed a set of parameters to draw on from the canvas
@@ -28,44 +69,28 @@ class DrawQueue:
         for render_callable in self.render_callables:
             render_callable(cairo_ctx)
 
+    operations = {
+        'paint': Paint,
+        'fill': Fill,
+        'set_source_rgb': SetSourceRgb,
+        'move_to': MoveTo,
+        'rectangle': Rectangle,
+    }
 
-#### drawing closures
-class paint(object):
-    def __call__(self, ctx):
-        ctx.paint()
+    def paint(self, *args):
+        self.append(Paint(*args))
 
-class fill(object):
-    def __call__(self, ctx):
-        ctx.fill()
+    def fill(self, *args):
+        self.append(Fill(*args))
 
-class set_source_rgb(object):
-    def __init__(self, r, g, b):
-        self.r = r
-        self.g = g
-        self.b = b
+    def set_source_rgb(self, *args):
+        self.append(SetSourceRgb(*args))
 
-    def __call__(self, ctx):
-        ctx.set_source_rgb(self.r, self.g, self.b)
+    def move_to(self, *args):
+        self.append(MoveTo(*args))
 
-
-class moveto(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __call__(self, ctx):
-        ctx.move_to(self.x, self.y)
-
-
-class rectangle(object):
-    def __init__(self, x, y, w, h):
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
-
-    def __call__(self, ctx):
-        ctx.rectangle(self.x, self.y, self.w, self.h)
+    def rectangle(self, *args):
+        self.append(Rectangle(*args))
 
 
 if __name__ == '__main__':
@@ -73,12 +98,12 @@ if __name__ == '__main__':
     dq = DrawQueue()
 
     # Add some commands to the drawing queue
-    dq.append(set_source_rgb_closure(1, 1, 1))
-    dq.append(paint_closure())
-    dq.append(moveto_closure(10, 0))
-    dq.append(rectangle_closure(0, 0, 20, 20))
-    dq.append(set_source_rgb_closure(0, 0, 0))
-    dq.append(fill_closure())
+    dq.set_source_rgb(1, 1, 1)
+    dq.paint()
+    dq.move_to(10, 0)
+    dq.rectangle(0, 0, 20, 20)
+    dq.set_source_rgb(0, 0, 0)
+    dq.fill()
 
     # Create a surface and context
     surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, 200, 200)
