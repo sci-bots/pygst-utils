7ee0fc0cb25a13324ecddf2929e8f4d50e566fa6
Christian Fobel <christian@fobel.net>
2014-03-03 11:14:31 -0500

    Remove frozen support, no need for EXE in Windows
    Remove frozen support, which eliminates the need for the Windows `pygst_utils`
    server packaged as an `exe`.

--- pygst_utils/bin/server.py
+++ pygst_utils/bin/server.py
@@ -2,7 +2,6 @@
 import logging
 import sys
 from subprocess import Popen
-import multiprocessing
 
 from path import path
 
@@ -10,13 +9,12 @@ from path import path
 def base_path():
     p = path(__file__).parent.abspath()
     attempted_paths = []
-    while p != path('/').abspath()\
-            and not p.joinpath('pygst_utils').isdir():
+    while p != path('/').abspath() and not p.joinpath('pygst_utils').isdir():
         attempted_paths.append(p)
         p = p.parent
     if not p:
-        raise RuntimeError, 'cannot find server.exe (attempted paths: %s)'\
-                % attempted_paths
+        raise RuntimeError('cannot find server.exe (attempted paths: %s)' %
+                           attempted_paths)
     return p.joinpath('pygst_utils')
 
 
@@ -31,17 +29,10 @@ def server_popen(port, force_aspect_ratio=False):
     command_args = [str(port)]
     if force_aspect_ratio:
         command_args.insert(0, '--force_aspect_ratio')
-    if hasattr(sys, 'frozen'):
-        import pygst_utils_windows_server
-        exe_path = path(pygst_utils_windows_server.__path__[0]).joinpath(
-                'server.exe')
-        print '[server_popen] exe_path=%s' % exe_path
-        server_process = Popen([exe_path] + command_args, cwd=exe_path.parent)
-    else:
-        script_path = base_path().joinpath('bin', 'server.py')
-        print '[server_popen] script_path=%s' % script_path
-        server_process = Popen([sys.executable, script_path] + command_args,
-                cwd=script_path.parent)
+    script_path = base_path().joinpath('bin', 'server.py')
+    print '[server_popen] script_path=%s' % script_path
+    server_process = Popen([sys.executable, script_path] + command_args,
+                           cwd=script_path.parent)
     return server_process
 
 
@@ -58,21 +49,23 @@ def parse_args():
 
 
 if __name__ == '__main__':
-    multiprocessing.freeze_support()
-
     logging.basicConfig(format='[%(levelname)s] %(message)s',
-            loglevel=logging.INFO)
+                        loglevel=logging.INFO)
 
     args = parse_args()
     print args
+
+    # Create a GStreamer window server process, with an initial `window_xid`
+    # set to zero.  The `window_xid` can be set via the server proxy API to
+    # specify which window buffer the GStreamer frames should be drawn to.
     service = WindowProcess(0, port=args.port,
-            force_aspect_ratio=args.force_aspect_ratio)
+                            force_aspect_ratio=args.force_aspect_ratio)
 
     logging.info('Starting server')
     service.start()
 
     port = service.parent_pipe.recv()
-    logging.info('Received port {} from child process'.format(port))
+    logging.info('Received port %s from child process', port)
 
     try:
         service.join()
