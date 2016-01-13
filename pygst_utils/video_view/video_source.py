8094260234f0fc3112f49abb01ca5c8388a16729
Christian Fobel <christian@fobel.net>
2016-01-13 05:53:52 -0500

    [NB] Add video source, writes webcam frames to 0MQ
    Add video source which writes video source (e.g., webcam) frames to
    ZeroMQ push socket.

---
+++ pygst_utils/video_view/video_source.py
@@ -0,0 +1,105 @@
+import sys
+import logging
+
+import zmq
+
+logger = logging.getLogger(__name__)
+
+from datetime import datetime
+import os, logging
+
+
+log = logging.getLogger( __name__ )
+HERE = os.path.dirname( __file__ )
+
+
+def main(pipeline_command, transport, host, port=None):
+    import pygst
+    pygst.require("0.10")
+    import gst
+
+    uri = '%s://%s:%s' % (transport, host, port)
+    ctx = zmq.Context.instance()
+    socket = zmq.Socket(ctx, zmq.PUSH)
+    socket.connect(uri)
+
+    pipeline = gst.parse_launch(pipeline_command)
+    app = pipeline.get_by_name('app-video')
+    status = {'frame_count': 0}
+
+    def on_new_buffer(appsink):
+        import numpy as np
+
+        buf = appsink.emit('pull-buffer')
+        caps = buf.caps[0]
+        width, height = caps['height'], caps['width']
+        status['frame_count'] += 1
+        status['buf'] = buf
+        channels = buf.size / (width * height)
+        socket.send_multipart([np.array([caps['height'], caps['width'],
+                                         channels], dtype='uint32').tostring(),
+                               buf.data])
+
+    app.connect('new-buffer', on_new_buffer)
+
+    pipeline.set_state(gst.STATE_PAUSED)
+    pipeline.set_state(gst.STATE_PLAYING)
+    return pipeline, status
+
+
+def update_status(status):
+    status['stop'] = datetime.now()
+    status['duration_s'] = (status['stop'] -
+                            status['start']).total_seconds()
+    status['fps'] = status['frame_count'] / status['duration_s']
+
+
+def parse_args(args=None):
+    """Parses arguments, returns (options, args)."""
+    from argparse import ArgumentParser
+
+    if args is None:
+        args = sys.argv
+
+    parser = ArgumentParser(description='GStreamer to ZeroMQ socket.')
+    log_levels = ('critical', 'error', 'warning', 'info', 'debug', 'notset')
+    parser.add_argument('-l', '--log-level', type=str, choices=log_levels,
+                        default='info')
+    parser.add_argument('transport')
+    parser.add_argument('host')
+
+    default_pipeline = [
+        'autovideosrc', '!',
+        'ffmpegcolorspace', '!',
+        'video/x-raw-rgb,format=RGB,framerate=30/1,width=640,height=480', '!',
+        #'video/x-raw-rgb,framerate=30/1,width=320,height=240', '!',
+        'videorate', '!',
+        'appsink',
+            'name=app-video',
+            #'enable-last-buffer=true',
+            'emit-signals=true',
+            #'sync=true',
+    ]
+    default_pipeline_command = " ".join(default_pipeline)
+    parser.add_argument('pipeline', nargs='?',
+                        default=default_pipeline_command,
+                        help='Default: %(default)s')
+    parser.add_argument('-p', '--port', default=None)
+
+    args = parser.parse_args()
+    args.log_level = getattr(logging, args.log_level.upper())
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    pipeline, status = main(args.pipeline, args.transport, args.host,
+                            args.port)
+
+    #while 'buf' not in status:
+        #time.sleep(.1)
+    #pipeline.set_state(gst.STATE_PAUSED)
+
+    #buf = status['buf']
+    #np_transform = np.identity(3, dtype='float32')
+    #cv_transform = cv.fromarray(np_transform)
