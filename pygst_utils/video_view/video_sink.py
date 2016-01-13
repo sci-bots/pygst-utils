40758c1dd9383e6b3eb614309141e195ecd48450
Christian Fobel <christian@fobel.net>
2016-01-13 07:21:38 -0500

    [WIP] Refactor video sink into VideoSink slave
    Refactor video sink into `VideoSink` slave.
    
    Currently, `VideoSink` contains no widgets, but periodically checks a
    ZeroMQ socket for incoming video frames.
    
    TODO
    ----
    
     - Add widget to set URI.

--- pygst_utils/video_view/video_sink.py
+++ pygst_utils/video_view/video_sink.py
@@ -8,6 +8,7 @@ from pygtkhelpers.ui.views.cairo_view import GtkCairoView
 from pygtkhelpers.utils import gsignal
 import cairo
 import cv2
+import gobject
 import gtk
 import numpy as np
 import zmq
@@ -50,50 +51,71 @@ class VideoInfo(SlaveView):
         self.label_dropped_frames.set_text('%d' % int(value))
 
 
-class VideoSink(GtkCairoView):
+class VideoSink(SlaveView):
     gsignal('frame-rate-update', float, int)
-    gsignal('frame-update', )
+    gsignal('frame-update', object)
 
-    def __init__(self):
+    def __init__(self, transport, target_host, port=None):
         self.status = {}
         super(VideoSink, self).__init__()
+        self.socket_info = {'host': target_host,
+                            'port': port,
+                            'transport': transport}
+        self.video_timeout_id = None
+        self._transform = np.identity(3, dtype='float32')
         self._shape = None
-        self.surfaces = self.get_surfaces()
+        self.frame_shape = None
+
+    @property
+    def transform(self):
+        return self._transform
+
+    @transform.setter
+    def transform(self, value):
+        self._transform = value
 
     @property
     def shape(self):
         if self._shape is not None:
             return self._shape
         elif 'np_warped_view' in self.status:
-            return self.status['np_warped_view']
+            return self.status['np_warped_view'].shape[:2]
 
-    def schedule_video_updates(self, transport, target_host, port=None,
-                               log_level=None):
-        if log_level is not None:
-            logging.basicConfig(level=log_level)
+    @shape.setter
+    def shape(self, value):
+        logging.info('[VideoSink] shape=%s', value)
+        self._shape = value
+        self.frame_shape = None
 
+    def reset(self):
+        if self.video_timeout_id is not None:
+            gobject.remove_source(self.video_timeout_id)
         self.ctx = zmq.Context.instance()
         self.target_socket = zmq.Socket(self.ctx, zmq.PULL)
-        base_uri = '%s://%s' % (transport, target_host)
+        base_uri = '%s://%s' % (self.socket_info['transport'],
+                                self.socket_info['host'])
+        port = self.socket_info['port']
         if port is None:
             port = self.target_socket.bind_to_random_port(base_uri)
             uri = '%s:%s' % (base_uri, port)
         else:
             uri = '%s:%s' % (base_uri, port)
             self.target_socket.bind(uri)
+        self.socket_info['port'] = port
         logger.info('Listening on: %s', uri)
 
         status = {'frame_count': 0, 'dropped_count': 0, 'start_time':
                   datetime.now()}
         self.status = status
-        self.video_timeout_id = gtk.timeout_add(10, self.update_status, status)
+        self.video_timeout_id = gtk.timeout_add(20, self.check_sockets, status)
 
-    def update_status(self, status):
+    def check_sockets(self, status):
         buf_str = None
         frame_count = 0
         while True:
             try:
-                shape, buf_str = self.target_socket.recv_multipart(zmq.NOBLOCK)
+                shape_str, buf_str = (self.target_socket
+                                      .recv_multipart(zmq.NOBLOCK))
                 frame_count += 1
             except zmq.Again:
                 break
@@ -104,13 +126,13 @@ class VideoSink(GtkCairoView):
                               status['start_time']).total_seconds()
 
         if buf_str is not None:
-            gtk.idle_add(self.render_base_surface, status, shape, buf_str)
+            self.prepare_np_frame(shape_str, buf_str)
 
         if status['duration'] > 2:
             status['fps'] = status['frame_count'] / status['duration']
             print '\r%5.1f frames/second (%2d dropped)' % (status['fps'],
-                                                        status
-                                                        ['dropped_count']),
+                                                           status
+                                                           ['dropped_count']),
             self.emit('frame-rate-update', status['fps'],
                       status['dropped_count'])
             status['frame_count'] = 0
@@ -118,29 +140,70 @@ class VideoSink(GtkCairoView):
             status['start_time'] = datetime.now()
         return True
 
-    def render_base_surface(self, status, shape, buf_str):
-        np_transform = np.identity(3, dtype='float32')
-        height, width, channels = np.frombuffer(shape, count=3,
+    def prepare_np_frame(self, shape_str, buf_str):
+        '''
+        Convert raw frame buffer to numpy array and apply warp perspective
+        transformation.
+
+        Emits:
+
+            frame-update : New numpy video frame available with perspective
+                transform applied.
+        '''
+        height, width, channels = np.frombuffer(shape_str, count=3,
                                                 dtype='uint32')
         im_buf = np.frombuffer(buf_str, dtype='uint8',
                                count=len(buf_str)).reshape(height, width, -1)
 
-        x, y, canvas_width, canvas_height = self.widget.get_allocation()
-        transform = np.identity(3, dtype='float32')
-        transform[0, 0] = canvas_width / float(width)
-        transform[1, 1] = canvas_height / float(height)
-        np_transform *= transform
-
+        # Cairo surface seems to use BGR ordering, so convert frame color.
         np_bgr = cv2.cvtColor(im_buf, cv2.COLOR_RGB2BGR)
-        np_warped = cv2.warpPerspective(np_bgr, np_transform, (canvas_width,
-                                                               canvas_height))
-        cr_warped, np_warped_view = np_to_cairo(np_warped)
-        status['cr_warped'] = cr_warped
-        status['np_warped_view'] = np_warped_view
+
+        # Warp and scale
+        if self.frame_shape != (width, height):
+            # Frame shape has changed.
+            self.frame_shape = width, height
+            if self.shape is None:
+                # No target shape has been set.  Use frame size.
+                self.scaled_transform = self.transform
+                self.shape = self.frame_shape
+                print self.scaled_transform
+            else:
+                # Update transform to scale to target shape.
+                self.scaled_transform = self.transform.copy()
+                scale = np.array(self.shape, dtype=float) / self.frame_shape
+                for i in xrange(2):
+                    self.scaled_transform[i, i] *= scale[i]
+                print self.scaled_transform
+        np_warped = cv2.warpPerspective(np_bgr, self.scaled_transform,
+                                        self.shape)
+        self.emit('frame-update', np_warped)
+
+
+class VideoView(GtkCairoView):
+    def __init__(self, transport, target_host, port=None):
+        self.socket_info = {'transport': transport,
+                            'host': target_host,
+                            'port': port}
+        super(VideoView, self).__init__()
+
+    def on_widget__configure_event(self, widget, event):
+        self.video_sink.shape = event.width, event.height
+
+    def create_ui(self):
+        self.video_sink = VideoSink(*[self.socket_info[k]
+                                      for k in ['transport', 'host', 'port']])
+        self.video_sink.reset()
+        self.video_sink.connect('frame-update', self.on_frame_update)
+        self.surfaces = self.get_surfaces()
+        super(VideoView, self).create_ui()
+
+    def on_frame_update(self, slave, np_frame):
+        if self.widget.window is None:
+            return
+        cr_warped, np_warped_view = np_to_cairo(np_frame)
 
         combined_surface = self.composite_surface([cr_warped] + self.surfaces)
         self.draw_surface(combined_surface)
-        return False
 
     def draw_to_widget_surface(self, surface):
         x, y, width, height = self.widget.get_allocation()
@@ -189,20 +252,30 @@ class VideoSink(GtkCairoView):
         return [surface1, surface2]
 
     def draw_surface(self, surface, operator=cairo.OPERATOR_OVER):
+        x, y, width, height = self.widget.get_allocation()
+        if width <= 0 and height <= 0:
+            return
         cairo_context = self.widget.window.cairo_create()
         cairo_context.set_operator(operator)
         cairo_context.set_source_surface(surface)
-        x, y, width, height = self.widget.get_allocation()
         cairo_context.rectangle(x, y, width, height)
         cairo_context.fill()
 
 
 class View(SlaveView):
+    def __init__(self, transport, target_host, port=None):
+        self.socket_info = {'transport': transport,
+                            'host': target_host,
+                            'port': port}
+        super(View, self).__init__()
+
     def create_ui(self):
         super(View, self).create_ui()
         self.debug_slave = self.add_slave(DebugView(), 'widget')
         self.info_slave = self.add_slave(VideoInfo(), 'widget')
-        self.video_slave = self.add_slave(VideoSink(), 'widget')
+        video_view = VideoView(*[self.socket_info[k]
+                                 for k in ['transport', 'host', 'port']])
+        self.video_slave = self.add_slave(video_view, 'widget')
 
         for widget in (self.debug_slave.widget, self.info_slave.widget):
             self.widget.set_child_packing(widget, False, False, 0,
@@ -210,12 +283,12 @@ class View(SlaveView):
 
         self.widget.set_child_packing(self.video_slave.widget, True, True, 0,
                                       gtk.PACK_START)
+        self.video_slave.video_sink.connect('frame-rate-update',
+                                            self.on_frame_rate_update)
 
-    def on_video_slave__frame_rate_update(self, slave, frame_rate,
-                                          dropped_frames):
+    def on_frame_rate_update(self, slave, frame_rate, dropped_frames):
         self.info_slave.frames_per_second = frame_rate
         self.info_slave.dropped_frames += dropped_frames
-
 
 
 def parse_args(args=None):
@@ -242,12 +315,8 @@ if __name__ == '__main__':
     args = parse_args()
 
     logging.basicConfig(level=args.log_level)
-    #run_video_sink(args.transport, args.host, port=args.port,
-                   #log_level=args.log_level)
 
-    view = View()
-    view.video_slave.schedule_video_updates(args.transport, args.host,
-                                            args.port, args.log_level)
+    view = View(args.transport, args.host, args.port)
 
     #gtk.timeout_add(1000, view.debug)
     view.widget.connect('destroy', gtk.main_quit)
